80aa011dd1e679912bc8571e59738de9367cef0b
Shohrux <shohruxnizomov170@gmail.com>
2026-05-04 09:27:07 +0500

    Foydalanuvchi ro‘yxatdan o‘tish (register) endpointi qo‘shildi

---
+++ src/modules/auth/auth.controller.js
@@ -0,0 +1,25 @@
+const authService = require("./auth.service");
+const { getRefreshCookieOptions } = require("../../utils/token");
+
+/**
+ * POST /api/auth/register
+ */
+const register = async (req, res, next) => {
+  try {
+    const { user, tokens } = await authService.register(req.body);
+
+    res.cookie("refreshToken", tokens.refreshToken, getRefreshCookieOptions());
+
+    return res.status(201).json({
+      success: true,
+      message: "Account created successfully",
+      data: {
+        user,
+        accessToken: tokens.accessToken,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
